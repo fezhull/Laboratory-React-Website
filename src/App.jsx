6ec937f76a44630067fa6e0bc19586f1f6f6d430
fezhull <felizhielerfezull@gmail.com>
2025-01-24 20:43:15 +0300

    Navbar changes

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <About id="about" />
       <Services id="services" />
       <Testimonial id="testimonial" />
-      <Contact />
+      <Contact id="contact" />
       <Footer />
     </div>
   );
